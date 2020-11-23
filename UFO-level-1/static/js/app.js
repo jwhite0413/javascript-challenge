8807b61ddf1668bf611d45bf4d24c14308e27de3
jwhite0413 <67512469+jwhite0413@users.noreply.github.com>
2020-11-22 19:37:04 -0500

    button working

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,73 +4,41 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 
-tableData.forEach((ufo) => {
-    console.log(ufo);
+//get data and show all on page
+tableData.forEach((ufodata) => {
+    console.log(ufodata);
     var row = tbody.append('tr');
 
-    Object.entries(ufo).forEach(([key, value]) => {
+    Object.entries(ufodata).forEach(([key, value]) => {
         console.log(key, value);
         var cell = row.append('td');
         cell.text(value);
     });
 });
 
-// var filterEntries = d3.select ("#filter-btn");
-// filterEntries.on("click", function() {
+var button = d3.select("#filter-btn");
+// var form = d3.select ("#form");
 
+// var submit = d3.select("#filter-btn");
+button.on("click", function() {
 
-//     d3.event.preventDefault();
+    // function runEnter() {
+    d3.event.preventDefault();
+    d3.select("tbody").html("");
 
+    var dateTime = d3.select("#datetime").property("value");
+    // console.log(dateTime);
 
-//     var date = d3.select("#datetime");
-//     var city = d3.select("#city");
-//     var state = d3.select("#state");
-//     var country = d3.select("#country");
-//     var shape = d3.select("#shape");
+    var filteredData = tableData.filter(record => record.datetime === dateTime);
+    // console.log(filteredData);
 
-//     d3.select("#datetime");
-//     d3.select("#city");
-//     d3.select("#state");
-//     d3.select("#country");
-//     d3.select("#shape");
+    filteredData.forEach((ufodata) => {
+        var row = tbody.append('tr');
 
-//     if (date !== ""|| city !== ""|| state !== ""||country !== ""|| shape !==""){
-//         buildTable(date,city,state,country,shape);
-//     }
-//     else{
-//         console.log("No input criteria provided.")
-//     };
-
-
-// function buildTable (date, city, state, country, shape) {
-//     tbody.html("");
-//     var filteredData =[];
-//     if (date !== "") {
-//         filteredData = sightings.filter(sighting => sighting.datetime == date);
-//     }
-//     else {
-//         filteredData = sightings;
-//     };
-
-//     if (city !== "") {
-//         filteredData = filteredData.filter(data =>data.city == city);
-//     };
-
-//     if (state !== "") {
-//         filteredData = filteredData.filter(data => data.state == state);
-//     };
-//     if (country !== "") {
-//         filteredData = filteredData.filter(data => data.country == country);
-//     };
-//     if (shape !== "") {
-//         filteredData = filteredData.filter(data => data.shape == shape);
-//     };
-//     console.log(filteredData);
-
-//     filteredData.forEach((sighting) => {
-//         var row = tbody.append("tr");
-//         Object.entries(sighting).forEach(([key,value]) => {
-//             var cell = row.append("td");
-//             cell.text(value);
-//         }
-//     }};
+        Object.entries(ufodata).forEach(([key, value]) => {
+            console.log(key, value);
+            var cell = row.append('td');
+            cell.text(value);
+        });
+    });
+});
